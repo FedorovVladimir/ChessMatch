359aefdda7af6ed6a536ab5afa6df2de4b11da22
Chessman97 <mikhailef@mail.ru>
2019-04-22 16:41:45 +0700

    Add Interface DataBase and class EmptyBase implements DataBase

---
+++ src/test/java/excel/TestExcel.java
@@ -0,0 +1,12 @@
+package excel;
+
+import org.junit.Test;
+import start.ExcelParser;
+
+public class TestExcel {
+    @Test
+    public void testCreateExcelParser(){
+        ExcelParser excelParser = new ExcelParser();
+    }
+
+}
