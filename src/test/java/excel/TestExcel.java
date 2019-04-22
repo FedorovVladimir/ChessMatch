7fd4ff921931dbd81f4b54ad70cac3c4016afc5d
Chessman97 <mikhailef@mail.ru>
2019-04-22 16:59:07 +0700

    Add test class. Create object and open file

--- src/test/java/excel/TestExcel.java
+++ src/test/java/excel/TestExcel.java
@@ -1,12 +1,23 @@
 package excel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 import start.ExcelParser;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class TestExcel {
     @Test
     public void testCreateExcelParser(){
         ExcelParser excelParser = new ExcelParser();
+    }
+    @Test
+    public void openExcelFile() throws IOException {
+        FileInputStream fileXls = new FileInputStream("C:\\Users\\Mikhail\\IdeaProjects\\ChessMatch\\src\\main\\resources\\excel\\Итоговая таблица.xls");
+        Workbook workbook = new HSSFWorkbook(fileXls);
     }
 
 }
