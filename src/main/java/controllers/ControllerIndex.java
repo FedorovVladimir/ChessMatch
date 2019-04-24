0e407419f78f12e26b0fdf4ad33252d26bcb15ba
Chessman97 <mikhailef@mail.ru>
2019-04-24 13:30:43 +0700

    Add template result.html and unload total table

--- src/main/java/controllers/ControllerIndex.java
+++ src/main/java/controllers/ControllerIndex.java
@@ -1,10 +1,21 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import start.DataBase;
+import start.EmptyDataBase;
 
 @Controller
 public class ControllerIndex {
+    private DataBase db = new EmptyDataBase();
+
+    @GetMapping("/result")
+    public String result(Model p) {
+        p.addAttribute("players", db.getMatches());
+        return "result";
+    }
+
     @GetMapping("/")
     public String index() {
         return "index";
